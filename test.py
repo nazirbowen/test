229f0908693638f2ab28b8a697a8f5732c1f72ea
nazirbowen <108888651+nazirbowen@users.noreply.github.com>
2023-04-28 06:11:04 -0700

    Add files via upload

---
+++ test.py
@@ -0,0 +1,32 @@
+1. What is a Python function? Describe a situation when you would use a
+function.
+#A function is a block of code that only runs when it is called,  a function can return data as a result.
+
+2. Describe the difference between a Python function and a Python method?
+#functions only run when called why methods can be used to call the function 
+
+3. What is a Python dictionary? Describe a scenario when you would use a
+dictionary.
+#Dictionaries are used to store data values in key pairs. They are written with curly brackets and values.
+
+4. Describe what GitHub is and provide 3 reasons why GitHub is an excellent tool for engineers
+#GitHub is a service for software development and coding. It provides the distributed version and the breaking down of code, bug tracking, and task management from how much it breaks down things.
+5. Describe the difference between a Python class and a Python dictionary. What are each used for and why?
+#A Python class is almost like a list of related things in 1. A Python dictionary is a list of things almost like an actual dictionary but code. 
+
+Code Response
+6. Create a line of code that contains a Python dictionary that has 5 cities you want to visit and their respective states.
+
+7. Create a line of code with a function that checks the length of the dictionary in the previous question.
+
+8. Create a line of code and write a function that takes 3 numbers that are provided from a user’s input, multiplies their inputs, and then returns the result.
+9. Create a line of code that contains a class called BankAccount that has attributes for account number, balance, owner name, and methods to deposit and withdraw money.
+
+Error handling and debugging Response
+
+10. Name 3 reasons why a piece of Python code may not work/ run an error
+message.
+#For 1 you might have a error in your code, The structure of your code might be wrong or maybe the code was just typed wrong.
+
+11. There are several issues preventing this code from working. What are the issues with this code and how can I fix this code to take in a user's name and display the print message?
+#There is a missing quotation mark before hello.
